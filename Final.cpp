f218bbc0c7f8d65fc7d78d560ccc092e9a9fe2ec
Rakshit Kapoor <500067642@stu.upes.ac.in>
2019-04-25 19:48:58 +0530

    New file created

---
+++ Final.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<windows.h>
+#include<conio.h>
+#include<fstream>
+#include<cstring>
+#include<cstdio>
+#include<cstdlib>
+#include<iomanip>
+using namespace std;
+//global variable declaration:
+int k=7,r=0,flag=0;
+COORD coord = {0, 0};
+
+void gotoxy(int x, int y)
+{
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
+struct date
+{
+    int mm,dd,yy;
+};
+
+ofstream fout;
+ifstream fin;
+
